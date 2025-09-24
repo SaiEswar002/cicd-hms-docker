a17a7f11c98a2541d6cefa32c4e5571efaa7740b
SaiEswar002 <saieswar2kg@gmail.com>
2025-09-24 09:12:08 +0530

    ..

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,5 +4,18 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // or '/' depending on your routing and folder structure
+  base: '/cicd-hms-docker', // or '/' depending on your routing and folder structure
+  server: {
+    host: true,
+    port: 3000,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8083/api/doctors',
+        changeOrigin: true
+      }
+    }
+  },
+  build: {
+    outDir: 'dist'
+  }
 })
